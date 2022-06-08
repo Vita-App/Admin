2e17fe9adeaee88572a6b43b00dac89c94772531
Rishabh Malhotra <rishabhmalhotraa01@gmail.com>
2022-06-08 14:18:44 +0530

    Adding Eslint Config

---
+++ eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["plugin:react/recommended", "xo", "eslint:recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "prefer-arrow"],
+  rules: {
+    indent: ["off"],
+    "object-curly-spacing": ["error", "always"],
+    camelcase: "off",
+    "capitalized-comments": "off",
+    "arrow-parens": "off",
+    "no-warning-comments": "off",
+    "no-unused-vars": "off",
+    "no-negated-condition": "off",
+    "@typescript-eslint/no-unused-vars": ["warn"],
+    "prefer-arrow/prefer-arrow-functions": [
+      "warn",
+      {
+        disallowPrototype: true,
+        singleReturnOnly: false,
+        classPropertiesAllowed: false,
+      },
+    ],
+    "no-mixed-operators": [
+      "warn",
+      {
+        groups: [
+          ["&", "|", "^", "~", "<<", ">>", ">>>"],
+          ["==", "!=", "===", "!==", ">", ">=", "<", "<="],
+          ["&&", "||"],
+          ["in", "instanceof"],
+        ],
+        allowSamePrecedence: false,
+      },
+    ],
+    "operator-linebreak": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
