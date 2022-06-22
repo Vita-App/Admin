fd4cbb7ded1455fab4aa815b10437c5cef7f50c0
Lalit <lalitkumarsingh3716@gmail.com>
2022-06-22 17:59:37 +0530

    Adding Authentication!

--- src/pages/Auth/Otp/index.tsx
+++ src/pages/Auth/Otp/index.tsx
@@ -1,8 +1,37 @@
-import React, { useMemo, useRef } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { TailSpin } from "react-loader-spinner";
+import axios from "axios";
+import { useQuery } from "react-query";
 import Container from "components/Container";
 import { Button, Stack, TextField, Typography } from "@mui/material";
+import { useSetRecoilState } from "recoil";
+import { useSnackbar } from "notistack";
+import { authState } from "store";
+import { useLocation, useNavigate } from "react-router";
+import { SERVER_URL } from "config.keys";
+
+const verifyOtp = async (otp: string, email?: string) => {
+  const { data } = await axios.post(`${SERVER_URL}/api/admin/verify-otp`, {
+    email,
+    otp,
+  });
+
+  return data;
+};
 
 const OtpPage = () => {
+  const { state } = useLocation();
+  const navigate = useNavigate();
+  const [otp, setOtp] = useState("");
+  const { enqueueSnackbar } = useSnackbar();
+  const { isLoading, data, isError, refetch } = useQuery(
+    ["verifyOtp", otp],
+    () => verifyOtp(otp, state as string),
+    {
+      enabled: false,
+    }
+  );
+  const setAuthState = useSetRecoilState(authState);
   const ref1 = useRef<HTMLInputElement>();
   const ref2 = useRef<HTMLInputElement>();
   const ref3 = useRef<HTMLInputElement>();
@@ -18,15 +47,28 @@ const OtpPage = () => {
     } else {
       refs[(index + 1) % 6].current!.focus();
     }
+
+    setOtp(refs.map((ref) => ref.current!.value).join(""));
   };
 
   const onSubmit = () => {
-    const otp = refs.map((ref) => ref.current!.value).join("");
-
     if (otp.length !== 6) return;
-
-    console.log(otp);
+    refetch();
   };
+
+  useEffect(() => {
+    if (data?.user) {
+      setAuthState({
+        isLoggedIn: true,
+        user: data.user,
+      });
+      navigate("/");
+    }
+
+    if (isError) {
+      enqueueSnackbar("Invalid or expired otp", { variant: "error" });
+    }
+  }, [data, isError, enqueueSnackbar, setAuthState, navigate]);
 
   return (
     <Container centered>
@@ -52,8 +94,17 @@ const OtpPage = () => {
           />
         ))}
       </Stack>
-      <Button variant="contained" color="success" onClick={onSubmit}>
-        Submit
+      <Button
+        variant="contained"
+        color="success"
+        onClick={onSubmit}
+        disabled={otp.length !== 6 || isLoading}
+      >
+        {isLoading ? (
+          <TailSpin width="25px" height="25px" color="#000" />
+        ) : (
+          "Submit"
+        )}
       </Button>
     </Container>
   );
