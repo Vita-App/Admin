e274d7e1b373cda8e7a9351fa5865272384e3744
Rishabh Malhotra <rishabhmalhotraa01@gmail.com>
2022-06-22 23:43:53 +0530

    prettier fixes

--- src/config.keys.ts
+++ src/config.keys.ts
@@ -1,2 +1,4 @@
-export const PRODUCTION = process.env.NODE_ENV === 'production';
-export const SERVER_URL = PRODUCTION ? 'https://vitaa-server.com' : 'http://localhost:5000';
+export const PRODUCTION = process.env.NODE_ENV === "production";
+export const SERVER_URL = PRODUCTION
+  ? "https://vitaa-server.com"
+  : "http://localhost:5000";
